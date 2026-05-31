7ba6fd296d8b1bb7ea7901c46ae7c5adb978e3e4
cdblue999 <cdblue999@gmail.com>
2026-05-31 11:15:33 +0200

    Add GPL v3 license and copyright headers

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+﻿/*
+ * Copyright (C) 2026 ZMS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 const SB_URL = 'https://amixcppknszjfscnepnx.supabase.co';
 const SB_KEY = 'sb_publishable_8pZgzv2BXthAUoBppO8U3A_edhabo2J';
 const supabaseClient = supabase.createClient(SB_URL, SB_KEY);
